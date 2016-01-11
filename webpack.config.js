e2bd60df46cd4828e9116655fa1501a2ea0add7a
Ike Peters <IkeP@Ikes-MacBook-Air.local>
2016-01-11 00:59:03 -0800

    adding webpack support

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var webpack = require('webpack');
+var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+
+module.exports = {
+    entry: './local_storage.js',
+    output: {
+        path: __dirname,
+        filename: 'local_storage.min.js'
+    },
+    module: {
+        loaders: [
+            { 
+                test: /\.js$/,
+                loader: 'babel',
+                exclude: /(node_modules|bower_components)/
+            }
+        ]
+    },
+    plugins: [
+        new UglifyJsPlugin({ minimize: true })
+    ]
+};
